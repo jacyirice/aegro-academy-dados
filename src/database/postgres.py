a45cb30a384961ae0ff0a1a87706fce48b1bdbad
Jacyirice Silva Oliveira <jacyirice.oliveira@aegro.com.br>
2022-05-12 15:55:27 -0300

    feat: Criando funções utilizando upsert

--- src/database/postgres.py
+++ src/database/postgres.py
@@ -2,15 +2,18 @@ import psycopg2
 from psycopg2.extensions import connection
 from psycopg2.extras import execute_values
 
+
 class PostgreSQL:
     conn: connection = None
 
-    def __init__(self, database: str, host: str, user: str, password: str, port: int = 5432) -> None:
+    def __init__(
+        self, database: str, host: str, user: str, password: str, port: int = 5432
+    ) -> None:
         """This a construct method
 
         Args:
             database (str): Database name
-            host (str): Database host 
+            host (str): Database host
             user (str): Database user
             password (str): Database password
             port (int, optional): Database port. Defaults to 5432.
@@ -48,12 +51,14 @@ class PostgreSQL:
         Returns:
             list: List containing table records
         """
-        cursor = self.conn.cursor()
-        cursor.execute(f"SELECT * FROM {table}")
-        objs = cursor.fetchall()
-        return objs
 
-    def select_by_field(self, table: str, field: str, value: str, op: str = '=') -> list:
+        with self.conn.cursor() as cursor:
+            cursor.execute(f"SELECT * FROM {table}")
+            return cursor.fetchall()
+
+    def select_by_field(
+        self, table: str, field: str, value: str, op: str = "="
+    ) -> list:
         """This method queries all records in a database table
         filtering by a column and a value
 
@@ -67,32 +72,37 @@ class PostgreSQL:
         Returns:
             list: List containing table records
         """
-        cursor = self.conn.cursor()
-        cursor.execute(f"SELECT * FROM {table} WHERE {field}{op}%s", (value,))
-        objs = cursor.fetchall()
-        return objs
+        with self.conn.cursor() as cursor:
+            cursor.execute(f"SELECT * FROM {table} WHERE {field}{op}%s", (value,))
+            return cursor.fetchall()
 
-    def insert(self, table: str, obj: dict) -> int:
-        cursor = self.conn.cursor()
+    def insert(self, table: str, obj: dict, returning_keys: str = "id") -> int:
         campos = self.format_fields(obj.keys())
         valores = self.format_values(obj.values())
 
-        cursor.execute(
-            f"INSERT INTO {table} ({campos}) VALUES ({('%s, '*len(valores))[:-2]}) RETURNING id;", valores)
-        self.conn.commit()
+        with self.conn.cursor() as cursor:
+            cursor.execute(
+                f"INSERT INTO {table} ({campos}) VALUES ({('%s, '*len(valores))[:-2]}) RETURNING {returning_keys};",
+                valores,
+            )
+            self.conn.commit()
 
-        return cursor.fetchone()[0]
+            return cursor.fetchone()[0]
 
-    def insert_many(self, table: str, list_fields: list, list_objs: list) -> list:
-        cursor = self.conn.cursor()
-
+    def insert_many(
+        self, table: str, list_fields: list, list_objs: list, returning_keys: str = "id"
+    ) -> list:
         campos = self.format_fields(list_fields)
         valores = self.format_many_values(list_objs)
 
-        execute_values(cursor,
-            f"INSERT INTO {table} ({campos}) VALUES %s RETURNING id;", valores)
-        self.conn.commit()
-        return cursor.fetchall()
+        with self.conn.cursor() as cursor:
+            execute_values(
+                cursor,
+                f"INSERT INTO {table} ({campos}) VALUES %s RETURNING {returning_keys};",
+                valores,
+            )
+            self.conn.commit()
+            return cursor.fetchall()
 
     def select_id_or_insert(self, table: str, uk: str, obj: dict) -> int:
         objs = self.select_by_field(table, uk, obj[uk])
@@ -102,6 +112,52 @@ class PostgreSQL:
         else:
             return objs[0][0]
 
+    def insert_or_update(
+        self,
+        table: str,
+        list_fields: list,
+        list_objs: list,
+        unique_key_name: str,
+        returning_keys: str = "id",
+    ) -> list:
+        campos = self.format_fields(list_fields)
+        upsert_update = self.format_upsert_update(list_fields)
+        valores = self.format_many_values(list_objs)
+
+        with self.conn.cursor() as cursor:
+            execute_values(
+                cursor,
+                f"""INSERT INTO {table} ({campos}) VALUES %s
+                    ON CONFLICT ({unique_key_name}) DO 
+                        UPDATE SET {upsert_update}
+                    RETURNING {returning_keys}""",
+                valores,
+            )
+            self.conn.commit()
+            return cursor.fetchall()
+
+    def insert_or_nothing(
+        self,
+        table: str,
+        list_fields: list,
+        list_objs: list,
+        unique_key_name: str,
+    ) -> None:
+        campos = self.format_fields(list_fields)
+        valores = self.format_many_values(list_objs)
+
+        with self.conn.cursor() as cursor:
+            execute_values(
+                cursor,
+                f"""INSERT INTO {table} ({campos}) VALUES %s
+                    ON CONFLICT ({unique_key_name}) DO NOTHING""",
+                valores,
+            )
+            self.conn.commit()
+
+    def format_upsert_update(self, campos):
+        return ", ".join([f"{c}=EXCLUDED.{c}" for c in campos])
+
     def format_many_values(self, list_objs) -> list:
         return [self.format_values(obj.values()) for obj in list_objs]
 
@@ -109,4 +165,4 @@ class PostgreSQL:
         return tuple(values)
 
     def format_fields(self, fields) -> str:
-        return ', '.join(fields)
+        return ", ".join(fields)
